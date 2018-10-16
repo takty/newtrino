49b3c0bc8d77716b19731c5d9ad44f6398ff94a3
Takty <tokutadaginaya@gmail.com>
2018-10-16 11:04:17 +0900

    Fix path definitions

--- src/topic/system/init.php
+++ src/topic/system/init.php
@@ -9,11 +9,11 @@ if (array_shift($ifs) === __FILE__) {
 }
 ?>
 <?php
-define('POST_PATH', __DIR__ . '/post/');
-define('DATA_PATH', __DIR__ . '/private/data/');
-define('LOG_FILE',  __DIR__ . '/private/var/log/log.txt');
+define('POST_PATH', __DIR__ . '/../post/');
+define('DATA_PATH', __DIR__ . '/../private/data/');
+define('LOG_FILE',  __DIR__ . '/../private/var/log/log.txt');
 
-define('DIC_PATH', 'private/php/igo/ipadic');
+// define('DIC_PATH', 'private/php/igo/ipadic');
 
 $dir = dirname($_SERVER['PHP_SELF']);
 $url = SERVER_HOST_URL . rtrim($dir, '/\\') . '/post/';
@@ -26,14 +26,14 @@ mb_http_output('utf-8');
 mb_http_input('utf-8');
 mb_regex_encoding('utf-8');
 
-require_once(__DIR__ . '/_lang.php');
-require_once(__DIR__ . '/private/php/util/TinySegmenter.php');
-require_once(__DIR__ . '/private/php/util/simple_html_dom.php');
-require_once(__DIR__ . '/private/php/Logger.php');
-require_once(__DIR__ . '/private/php/Store.php');
-require_once(__DIR__ . '/private/php/Post.php');
-require_once(__DIR__ . '/private/php/Media.php');
-require_once(__DIR__ . '/private/php/Indexer.php');
+require_once(__DIR__ . '/lang.php');
+require_once(__DIR__ . '/../private/php/util/TinySegmenter.php');
+require_once(__DIR__ . '/../private/php/util/simple_html_dom.php');
+require_once(__DIR__ . '/../private/php/Logger.php');
+require_once(__DIR__ . '/../private/php/Store.php');
+require_once(__DIR__ . '/../private/php/Post.php');
+require_once(__DIR__ . '/../private/php/Media.php');
+require_once(__DIR__ . '/../private/php/Indexer.php');
 
 if (!function_exists('_h')) {function _h($str) {return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');}}
 if (!function_exists('_eh')) {function _eh($str) {echo htmlspecialchars($str, ENT_QUOTES, 'UTF-8');}}
