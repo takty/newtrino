bde2a90eba71057019319d882ac1d4dc62f5ae20
Takty <tokutadaginaya@gmail.com>
2018-10-19 11:13:53 +0900

    Improve the editor size

--- src/topic/private/js/edit.js
+++ src/topic/private/js/edit.js
@@ -4,19 +4,19 @@
  *
  * @author Takuto Yanagida @ Space-Time Inc.
  * @author Yusuke Manabe @ Space-Time Inc.
- * @version 2018-10-18
+ * @version 2018-10-19
  *
  */
 
 
 function initEdit() {
-	var fp = flatpickr('#post_published_date', {
+	const fp = flatpickr('#post_published_date', {
 		enableSeconds: true, time_24hr: true, enableTime: true,
 		onChange: function(dateObj, dateStr, instance) {
 			if (document.getElementById('post_state').value === 'draft') return;
-			var s = dateStr.replace(/-|:|\s/g, '');
-			var dn = parseInt(s, 10);
-			var cn = parseInt(formatDate(new Date(), 'YYYYMMDDhhmmss'));
+			const s = dateStr.replace(/-|:|\s/g, '');
+			const dn = parseInt(s, 10);
+			const cn = parseInt(formatDate(new Date(), 'YYYYMMDDhhmmss'));
 			if (dn <= cn) {
 				document.getElementById('post_state_published').selected = true;
 			} else {
@@ -27,17 +27,17 @@ function initEdit() {
 	document.getElementById('post_state').addEventListener('change', function () {
 		if (this.value === 'draft') return;
 		if (this.value === 'published') {
-			var s = document.getElementById('post_published_date').value.replace(/-|:|\s/g, '');
-			var dn = parseInt(s, 10);
-			var cn = parseInt(formatDate(new Date(), 'YYYYMMDDhhmmss'));
+			const s = document.getElementById('post_published_date').value.replace(/-|:|\s/g, '');
+			const dn = parseInt(s, 10);
+			const cn = parseInt(formatDate(new Date(), 'YYYYMMDDhhmmss'));
 			if (dn > cn) {
 				document.getElementById('post_published_date').value = formatDate(new Date(), 'YYYY-MM-DD hh:mm:ss');
 			}
 		}
 		if (this.value === 'reserved') {
-			var s = document.getElementById('post_published_date').value.replace(/-|:|\s/g, '');
-			var dn = parseInt(s, 10);
-			var cn = parseInt(formatDate(new Date(), 'YYYYMMDDhhmmss'));
+			const s = document.getElementById('post_published_date').value.replace(/-|:|\s/g, '');
+			const dn = parseInt(s, 10);
+			const cn = parseInt(formatDate(new Date(), 'YYYYMMDDhhmmss'));
 			if (dn <= cn) {
 				setTimeout(function () {fp.open();}, 100);
 			}
@@ -53,16 +53,12 @@ function initEdit() {
 		toolbar: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
 		content_css: '../editor-style.css',
 		language: 'ja',
-		setup: function (ed) {
-			ed.on('change', function (ed) {
-				changed = true;
-			});
-		}
+		setup: function (ed) { ed.on('change', function (ed) { changed = true; }); },
 	});
 	document.getElementById('post_title').addEventListener('change', function () {
 		if (this.value !== '') {
-			var elms = document.getElementsByClassName('message');
-			for (var i = 0; i < elms.length; i += 1) elms[i].style.display = '';
+			const elms = document.getElementsByClassName('message');
+			for (let i = 0; i < elms.length; i += 1) elms[i].style.display = '';
 		}
 		onChanged();
 	});
@@ -74,12 +70,12 @@ function initEdit() {
 	});
 	document.getElementById('post_cat').addEventListener('change', function () {
 		onChanged();
-		var isEvent = document.getElementById('post_cat').value === 'event';
-		document.getElementById('frame-event-duration').style.display = isEvent ? 'block' : 'none';
+		const isEvent = document.getElementById('post_cat').value === 'event';
+		document.getElementById('frame-event-duration').style.display = (isEvent ? 'block' : 'none');
 	});
 	window.addEventListener('beforeunload', function (e) {
 		if (changed) {
-			var msg = 'Do you want to move from the page you are inputting?';
+			const msg = 'Do you want to move from the page you are inputting?';
 			e.returnValue = msg;
 			return msg;
 		}
@@ -87,23 +83,33 @@ function initEdit() {
 	flatpickr('#event_date_bgn', {});
 	flatpickr('#event_date_end', {});
 
-	var isEvent = document.getElementById('post_cat').value === 'event';
-	document.getElementById('frame-event-duration').style.display = isEvent ? 'block' : 'none';
+	const isEvent = document.getElementById('post_cat').value === 'event';
+	document.getElementById('frame-event-duration').style.display = (isEvent ? 'block' : 'none');
+
+	function onResize() {
+		const clm = document.querySelector('.column-main');
+		const div = document.querySelector('.column-main .btn-row + div');
+		const r = div.getBoundingClientRect();
+		const h = clm.clientHeight - (0 | r.top);
+		tinymce.activeEditor.theme.resizeTo(null, h);
+	}
+	window.addEventListener('resize', onResize);
+	setTimeout(onResize, 100);
 }
 
-var changed = false;
+let changed = false;
 
-var onChanged = function () {
+const onChanged = function () {
 	if (changed === false) {
 		changed = true;
-		var elms = document.getElementsByClassName('message');
-		for (var i = 0; i < elms.length; i += 1) elms[i].style.display = '';
-		var um = document.getElementById('update-msg');
+		const elms = document.getElementsByClassName('message');
+		for (let i = 0; i < elms.length; i += 1) elms[i].style.display = '';
+		const um = document.getElementById('update-msg');
 		um.innerText = '';
 	}
 }
 
-var formatDate = function(date, format) {
+const formatDate = function (date, format) {
 	if (!format) format = 'YYYY-MM-DD hh:mm:ss';
 	format = format.replace(/YYYY/g, date.getFullYear());
 	format = format.replace(/MM/g, ('0' + (date.getMonth() + 1)).slice(-2));
@@ -116,7 +122,7 @@ var formatDate = function(date, format) {
 
 function showPreview() {
 	window.open('about:blank', 'preview');
-	var fp = document.getElementById('form-post');
+	const fp = document.getElementById('form-post');
 	fp.target = 'preview';
 	fp.action = 'preview.php';
 	fp.submit();
@@ -124,12 +130,12 @@ function showPreview() {
 
 function update() {
 	if (document.getElementById('post_title').value === '') {
-		var elm = document.getElementById('message_enter_title');
+		const elm = document.getElementById('message_enter_title');
 		elm.style.display = 'block';
 		return false;
 	}
 	document.getElementById('mode').value = 'update';
-	var fp = document.getElementById('form-post');
+	const fp = document.getElementById('form-post');
 	fp.target = '';
 	fp.action = 'edit.php';
 	changed = false;
@@ -137,23 +143,23 @@ function update() {
 }
 
 function showList() {
-	var fp = document.getElementById('form-post');
+	const fp = document.getElementById('form-post');
 	fp.target = '';
 	fp.action = 'index.php';
 	fp.submit();
 }
 
 function showPost() {
-	var id = document.getElementById('id').value;
+	const id = document.getElementById('id').value;
 	window.open('../view.php?id=' + id, 'post', '');
 }
 
 function showMediaChooser() {
-	var sid = document.getElementById('sid').value;
-	var pid = document.getElementById('id').value;
-	var dialogPlaceholder = document.getElementById('dialog-placeholder');
+	const sid = document.getElementById('sid').value;
+	const pid = document.getElementById('id').value;
+	const dialogPlaceholder = document.getElementById('dialog-placeholder');
 
-	var win = document.createElement('iframe');
+	const win = document.createElement('iframe');
 	win.id = 'mediaChooser';
 	win.src = 'media.php?sid=' + sid + '&id=' + pid;
 	win.style.width = '800px';
@@ -164,17 +170,17 @@ function showMediaChooser() {
 }
 
 function closeMediaChooser() {
-	var dialogPlaceholder = document.getElementById('dialog-placeholder');
-	var win = document.getElementById('mediaChooser');
+	const dialogPlaceholder = document.getElementById('dialog-placeholder');
+	const win = document.getElementById('mediaChooser');
 	dialogPlaceholder.removeChild(win);
 	dialogPlaceholder.style.display = 'none';
 }
 
-var wh_min = 220;
+const wh_min = 220;
 
 function insertMedia(name, src, w, h, align, size, isImage) {
 	closeMediaChooser();
-	var cs = [];
+	const cs = [];
 	if (align === 'l') cs.push('alignleft');
 	if (align === 'r') cs.push('alignright');
 	if (align === 'c') cs.push('aligncenter');
@@ -186,27 +192,27 @@ function insertMedia(name, src, w, h, align, size, isImage) {
 		if (size === 'm') cs.push('size-medium');
 		if (size === 's') cs.push('size-small');
 
-		var imgstr = '<a href="' + src + '" class="' + cs.join(' ') + '"><img src="' + src + '"';
+		let imgstr = '<a href="' + src + '" class="' + cs.join(' ') + '"><img src="' + src + '"';
 		if (size !== 'o') {  // Not Original Size
-			var r = wh_min
+			let r = wh_min
 			if (size === "l") r *= 3;
 			if (size === "m") r *= 2;
-			var vw = Math.round(getSizeW(r, w, h));
-			var vh = Math.round(getSizeH(r, w, h));
-			var is = ' width="' + vw + '" height="' + vh + '" ';
+			const vw = Math.round(getSizeW(r, w, h));
+			const vh = Math.round(getSizeH(r, w, h));
+			const is = ' width="' + vw + '" height="' + vh + '" ';
 			imgstr = imgstr + is;
 		}
 		imgstr += "></a>";
 		tinymce.activeEditor.execCommand('mceInsertContent', false, imgstr);
 	} else {
-		var linkStr = '<a href="' + src + '" class="' + cs.join(' ') + '">' + name + '</a>';
+		const linkStr = '<a href="' + src + '" class="' + cs.join(' ') + '">' + name + '</a>';
 		tinymce.activeEditor.execCommand('mceInsertContent', false, linkStr);
 	}
 }
 
 function getSizeW(wh, w, h) {
 	if (w > h) {
-		var p = parseFloat(w) / parseFloat(h);
+		const p = parseFloat(w) / parseFloat(h);
 		return parseFloat(wh) * parseFloat(p);
 	} else {
 		return wh;
@@ -217,7 +223,7 @@ function getSizeH(wh, w, h) {
 	if (w > h) {
 		return wh;
 	} else {
-		var p = parseFloat(h) / parseFloat(w);
+		const p = parseFloat(h) / parseFloat(w);
 		return parseFloat(wh) * parseFloat(p);
 	}
 }
