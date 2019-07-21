38971ffb4d8346ee9b08cb53cf846e7332c640eb
Takuto Yanagida <yanagida@stxst.com>
2019-07-22 08:52:41 +0900

    Refactoring

--- src/topic/core/class-logger.php
+++ src/topic/core/class-logger.php
@@ -16,39 +16,40 @@ class Logger {
 	const DEBUG_VIEW = false;
 	const LOG_FILE   = __DIR__ . '/var/log/log.txt';
 
-	static function output($msg) {
+	static function output( $msg ) {
 		$path = self::LOG_FILE;
+		self::ensureFile( $path );
+		$fp = fopen( $path, 'ab+' );
+		if ( $fp === false ) return;
 
-		if (file_exists($path)) {
-			$fsize = filesize($path);
-			if ($fsize > self::MAX_SIZE) {
-				$pi = pathinfo($path);
+		flock( $fp, LOCK_EX );
+		set_file_buffer( $fp, 0 );
+		$line = join( ' ', [ date('Y-m-d H:i:s'), getenv('REMOTE_USER'), $msg ] );
+		if ( self::DEBUG_VIEW ) print( "$line\n" );
+		fputs( $fp, "$line\n" );
+		flock( $fp, LOCK_UN );
+		fclose( $fp );
+	}
+
+	static private function ensureFile( $path ) {
+		if ( file_exists( $path ) ) {
+			$fsize = filesize( $path );
+			if ( self::MAX_SIZE < $fsize ) {
+				$pi  = pathinfo( $path );
 				$ext = '.' . $pi['extension'];
 				$fn  = $pi['filename'];
 				$dir = $pi['dirname'] . '/';
-				self::rotateFile($dir, $fn, $ext);
+				self::rotateFile( $dir, $fn, $ext );
 			}
 		} else {
-			$dir = dirname($path);
-			if (!file_exists($dir)) {
-				if (mkdir($dir, 0755, true)) chmod($dir, 0755);
+			$dir = dirname( $path );
+			if ( ! file_exists( $dir ) ) {
+				if ( mkdir( $dir, 0755, true ) ) chmod( $dir, 0755 );
 			}
-		}
-		$fp = fopen($path, 'ab+');
-		if ($fp) {
-			flock($fp, LOCK_EX);
-			set_file_buffer($fp, 0);
-			$line = join(' ', [date('Y-m-d H:i:s'), getenv('REMOTE_USER'), $msg]);
-			if (self::DEBUG_VIEW) {
-				print($line . "\n");
-			}
-			fputs($fp, $line . "\n");
-			flock($fp, LOCK_UN);
-			fclose($fp);
 		}
 	}
 
-	static function rotateFile( $dir, $fn, $ext ) {
+	static private function rotateFile( $dir, $fn, $ext ) {
 		if ( file_exists( "$dir{$fn}[5]$ext" ) ) {
 			unlink( "$dir{$fn}[5]$ext" );
 		}
