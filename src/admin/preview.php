356326368dd72b9743f4822f45434c1410974a1e
Takuto Yanagida <yanagida@stxst.com>
2020-07-15 17:06:33 +0900

    Improve admin preview dialog

--- src/admin/preview.php
+++ src/admin/preview.php
@@ -5,7 +5,7 @@ namespace nt;
  * Preview
  *
  * @author Takuto Yanagida @ Space-Time Inc.
- * @version 2020-07-12
+ * @version 2020-07-14
  *
  */
 
@@ -14,7 +14,7 @@ require_once( __DIR__ . '/index.php' );
 
 
 $t_title   = $nt_q['post_title'];
-$t_pdate   = $nt_q['post_date'];
+$t_date    = $nt_q['post_date'];
 $t_content = $nt_q['post_content'];
 
 global $nt_store;
@@ -26,9 +26,9 @@ foreach ( $taxes as $tax => $data ) {
 	$ls = [];
 	foreach ( $ts as $t ) {
 		$l = $nt_store->taxonomy()->getTermLabel( $tax, $t );
-		if ( ! empty( $l ) ) $ls[] = $l;
+		if ( ! empty( $l ) ) $ls[] = _h( $l );
 	}
-	if ( ! empty( $ls ) ) $tax_labels[ $tax ] = implode( ', ', $ls );
+	if ( ! empty( $ls ) ) $tax_labels[ $tax ] = '<li>' . implode( '</li><li>', $ls ) . '</li>';
 }
 
 
@@ -39,22 +39,20 @@ header('Content-Type: text/html;charset=utf-8');
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title><?= _ht('Preview') ?> - Newtrino</title>
-<link rel="stylesheet" media="all" href="css/style.min.css" />
+<title><?= _ht( 'Preview' ) ?> - Newtrino</title>
+<link rel="stylesheet" media="all" href="../data/preview.css" />
 </head>
 <body class="preview">
 <div class="container">
-	<h1>Newtrino</h1>
-	<h2><?= _ht('Preview') ?></h2>
-	<main class="topic-post">
-		<header>
-			<h3><?= _h($t_title) ?></h3>
-			<span class="date"><?= _h($t_pdate) ?></span>
-<?php foreach ( $tax_labels as $tax => $labels ) : ?>
-			<span class="taxonomy-<?= _h( $tax ) ?>"> [<?= _h( $labels  ) ?>]</span>
+	<header class="entry-header">
+		<h1><?= _h( $t_title ) ?></h1>
+		<div class="date"><?= _h( $t_date ) ?></div>
+<?php foreach ( $tax_labels as $tax => $lis ) : ?>
+		<ul class="taxonomy-<?= _h( $tax ) ?>"><?= $lis ?></ul>
 <?php endforeach; ?>
-		</header>
-		<div class="post-content"><?= $t_content ?></div>
+	</header>
+	<main class="entry-content">
+		<?= $t_content ?>
 	</main>
 </div>
 </body>
