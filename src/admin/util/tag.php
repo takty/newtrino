ebdfa98d430d38968c81ecb9730def699e89f738
Takuto Yanagida <yanagida@stxst.com>
2020-07-24 18:56:18 +0900

    Refactoring

--- src/admin/util/tag.php
+++ src/admin/util/tag.php
@@ -5,7 +5,7 @@ namespace nt;
  * Functions for Output
  *
  * @author Takuto Yanagida @ Space-Time Inc.
- * @version 2020-07-23
+ * @version 2020-07-24
  *
  */
 
@@ -18,21 +18,15 @@ function _eh( string $str ) {
 	echo htmlspecialchars( $str, ENT_QUOTES, 'UTF-8' );
 }
 
-function _ht( string $str, string $context = 'default' ): string {
-	return htmlspecialchars( translate( $str, $context ), ENT_QUOTES, 'UTF-8' );
+function _ht( string $str ): string {
+	return htmlspecialchars( translate( $str ), ENT_QUOTES, 'UTF-8' );
 }
 
-function translate( string $str, string $context = 'default' ): string {
-	if ( defined( 'NT_ADMIN' ) && $context === 'default' ) {
-		$context = 'admin';
-	}
+function translate( string $str ): string {
 	global $nt_res;
-	if ( isset( $nt_res[ $context ][ $str ] ) ) {
-		return $nt_res[ $context ][ $str ];
-	}
-	if ( $context !== 'default' ) {
-		if ( isset( $nt_res['default'][ $str ] ) ) {
-			return $nt_res['default'][ $str ];
+	foreach ( $nt_res as $key => $vals ) {
+		if ( isset( $vals[ $str ] ) ) {
+			return $vals[ $str ];
 		}
 	}
 	return $str;
