2055c9f524711670e983f02fd86a433790772059
Takuto Yanagida <yanagida@stxst.com>
2020-07-25 00:31:04 +0900

    Improve preview dialog

---
+++ src/admin/handler-preview.php
@@ -0,0 +1,44 @@
+<?php
+namespace nt;
+/**
+ *
+ * Handler - Preview
+ *
+ * @author Takuto Yanagida @ Space-Time Inc.
+ * @version 2020-07-25
+ *
+ */
+
+
+require_once( __DIR__ . '/index.php' );
+require_once( __DIR__ . '/../core/class-store.php' );
+require_once( __DIR__ . '/../core/util/template.php' );
+
+start_session( true, true );
+
+
+function handle_query( array $q ) {
+	$q_title   = $q['post_title']   ?? '';
+	$q_date    = $q['post_date']    ?? '';
+	$q_content = $q['post_content'] ?? '';
+
+	global $nt_store;
+	$taxes = array_keys( $nt_store->taxonomy()->getTaxonomyAll() );
+	$taxes = [];
+	foreach ( $taxes as $tax ) {
+		if ( ! isset( $q["taxonomy:$tax"] ) ) continue;
+		$ts = is_array( $q["taxonomy:$tax"] ) ? $q["taxonomy:$tax"] : [ $q["taxonomy:$tax"] ];
+		$ls = [];
+		foreach ( $ts as $t ) {
+			$ls[] = _h( $nt_store->taxonomy()->getTermLabel( $tax, $t ) );
+		}
+		if ( ! empty( $ls ) ) $taxes[] = [ 'taxonomy' => $tax, 'term_labels' => $ls ];
+	}
+
+	return [
+		'title'      => $q_title,
+		'date'       => $q_date,
+		'content'    => $q_content,
+		'taxonomies' => $taxes,
+	];
+}
--- src/admin/preview.php
+++ src/admin/preview.php
@@ -5,32 +5,13 @@ namespace nt;
  * Preview
  *
  * @author Takuto Yanagida @ Space-Time Inc.
- * @version 2020-07-24
+ * @version 2020-07-25
  *
  */
 
 
-require_once( __DIR__ . '/index.php' );
-require_once( __DIR__ . '/../core/class-store.php' );
-
-start_session( true );
-
-$q = $_REQUEST;
-$q_title   = $q['post_title']   ?? '';
-$q_date    = $q['post_date']    ?? '';
-$q_content = $q['post_content'] ?? '';
-
-$taxes = array_keys( $nt_store->taxonomy()->getTaxonomyAll() );
-$tax_lis = [];
-foreach ( $taxes as $tax ) {
-	if ( ! isset( $q["taxonomy:$tax"] ) ) continue;
-	$ts = is_array( $q["taxonomy:$tax"] ) ? $q["taxonomy:$tax"] : [ $q["taxonomy:$tax"] ];
-	$ls = [];
-	foreach ( $ts as $t ) {
-		$ls[] = _h( $nt_store->taxonomy()->getTermLabel( $tax, $t ) );
-	}
-	if ( ! empty( $ls ) ) $tax_lis[ $tax ] = '<li>' . implode( '</li><li>', $ls ) . '</li>';
-}
+require_once( __DIR__ . '/handler-preview.php' );
+$v = handle_query( $_REQUEST );
 
 
 header('Content-Type: text/html;charset=utf-8');
@@ -40,21 +21,27 @@ header('Content-Type: text/html;charset=utf-8');
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title><?= _ht( 'Preview' ) ?> - Newtrino</title>
 <link rel="stylesheet" media="all" href="../data/preview.css" />
+<title><?= _ht( 'Preview' ) ?> - Newtrino</title>
 </head>
 <body class="preview">
+
+<?php \nt\begin(); ?>
 <div class="container">
 	<header class="entry-header">
-		<h1><?= _h( $q_title ) ?></h1>
-		<div class="date"><?= _h( $q_date ) ?></div>
-<?php foreach ( $tax_lis as $tax => $lis ) : ?>
-		<ul class="taxonomy-<?= _h( $tax ) ?>"><?= $lis ?></ul>
-<?php endforeach; ?>
+		<h1>{{title}}</h1>
+		<div class="date">{{title}}</div>
+{{#taxonomies}}
+		<ul class="taxonomy-{{taxonomy}}">
+			{{#term_labels}}<li>{{.}}</li>{{/term_labels}}
+		</ul>
+{{/taxonomies}}
 	</header>
 	<main class="entry-content">
-		<?= $q_content ?>
+		{{{content}}}
 	</main>
 </div>
+<?php \nt\end( $v ); ?>
+
 </body>
 </html>
