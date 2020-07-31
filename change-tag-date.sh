278bd9863b913c8f76265f3850fd356b719d61ae
Takuto Yanagida <yanagida@stxst.com>
2020-08-01 02:05:06 +0900

    Add a shell script for fix git tag date

---
+++ change-tag-date.sh
@@ -0,0 +1,1 @@
+git tag -l | while read -r tag ; do COMMIT_HASH=$(git rev-list -1 $tag) && GIT_COMMITTER_DATE="$(git show $COMMIT_HASH --format=%aD | head -1)" git tag -a -f $tag -m"$tag" $COMMIT_HASH ; done && git push --tags --force
