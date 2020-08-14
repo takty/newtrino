96f9f144974a42aef062802a8cb577ca4032445c
Takuto Yanagida <yanagida@stxst.com>
2020-08-14 15:13:24 +0900

    Minor fix of path

--- src/core/index.php
+++ src/core/index.php
@@ -5,7 +5,7 @@ namespace nt;
  * Definitions of Constants and Functions
  *
  * @author Takuto Yanagida @ Space-Time Inc.
- * @version 2020-08-04
+ * @version 2020-08-14
  *
  */
 
@@ -17,16 +17,16 @@ Logger::$debug = true;
 
 
 define( 'NT_DIR', pathinfo( __DIR__, PATHINFO_DIRNAME ) . '/' );
-define( 'NT_DIR_DATA', NT_DIR . '/data/' );
-define( 'NT_DIR_POST', NT_DIR . '/post/' );
+define( 'NT_DIR_DATA', NT_DIR . 'data/' );
+define( 'NT_DIR_POST', NT_DIR . 'post/' );
 
 define( 'NT_URL_HOST', ( empty( $_SERVER['HTTPS'] ) ? 'http://' : 'https://' ) . $_SERVER['HTTP_HOST'] );
 define( 'NT_URL', NT_URL_HOST . get_url_from_path( NT_DIR ) . '/' );
 define( 'NT_URL_POST', NT_URL_HOST . get_url_from_path( NT_DIR_POST ) . '/' );
 
 if ( defined( 'NT_ADMIN' ) ) {
-	define( 'NT_DIR_ADMIN', NT_DIR . '/admin/' );
-	define( 'NT_DIR_ADMIN_RES', NT_DIR . '/admin/res/' );
+	define( 'NT_DIR_ADMIN', NT_DIR . 'admin/' );
+	define( 'NT_DIR_ADMIN_RES', NT_DIR . 'admin/res/' );
 	define( 'NT_DIR_SESSION', NT_DIR_ADMIN . 'var/session/' );
 
 	define( 'NT_URL_ADMIN', NT_URL_HOST . get_url_from_path( NT_DIR_ADMIN ) . '/' );
