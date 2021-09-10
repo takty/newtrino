97d22e167fa13c70c7f4ff559bc94a70eabac94d
Takuto Yanagida <yanagida@stxst.com>
2021-09-11 08:18:16 +0900

    Add variable checking

--- src/core/util/label.php
+++ src/core/util/label.php
@@ -5,20 +5,24 @@ namespace nt;
  * Functions for Labels
  *
  * @author Takuto Yanagida
- * @version 2020-07-31
+ * @version 2021-09-11
  *
  */
 
 
 function normalize_label( array &$d, string $l ): void {
-	if ( isset( $d[ "label@$l" ] ) )    $d['label']    = $d[ "label@$l" ];
-	if ( isset( $d[ "sg_label@$l" ] ) ) $d['sg_label'] = $d[ "sg_label@$l" ];
+	if ( isset( $d[ "label@$l" ] ) ) {
+		$d['label'] = $d[ "label@$l" ];
+	}
+	if ( isset( $d[ "sg_label@$l" ] ) ) {
+		$d['sg_label'] = $d[ "sg_label@$l" ];
+	}
 
 	if ( ! isset( $d['label'] ) && isset( $d['sg_label'] ) ) {
 		$d['label'] = $d['sg_label'];
-	} else if ( isset( $d['label'] ) && ! isset( $d['sg_label'] ) ) {
+	} elseif ( isset( $d['label'] ) && ! isset( $d['sg_label'] ) ) {
 		$d['sg_label'] = $d['label'];
-	} else if ( ! isset( $d['label'] ) && ! isset( $d['sg_label'] ) ) {
+	} elseif ( ! isset( $d['label'] ) && ! isset( $d['sg_label'] ) && isset( $d['slug'] ) ) {
 		$t = ucwords( str_replace( '_', ' ', $d['slug'] ) );
 		$d['label']    = $t;
 		$d['sg_label'] = $t;
