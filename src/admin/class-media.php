1d66c921a61a0df6ef97f6f210325500cd0e5da6
Takuto Yanagida <yanagida@stxst.com>
2022-12-17 00:21:56 +0900

    Improve and refactor media class

--- src/admin/class-media.php
+++ src/admin/class-media.php
@@ -16,21 +16,69 @@ require_once( __DIR__ . '/util/file.php' );
 
 class Media {
 
+	private const SIZE_NAME_MIN  = 'small';
+	private const SIZE_NAME_ORIG = 'full';
+
+	private const IMG_EXTS  = [ 'png', 'jpeg', 'jpg' ];
+	private const JSON_OPTS = JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE;
+
+	private const FILTER_MAT = [ [ 0, -1, 0 ], [ -1, 12, -1 ], [ 0, -1, 0 ] ];
+	private const FILTER_DIV = 8;
+
 	private $_id;
 	private $_dir;
 	private $_url;
 	private $_meta;
 	private $_data;
 	private $_ntUrl;
+	private $_sizes;
 
 	public function __construct( string $id, string $mediaDirName, string $ntUrl ) {
-		global $nt_store;
-		$this->_id   = $id;
-		$this->_dir  = $nt_store->getPostDir( $id, null ) . $mediaDirName . '/';
-		$this->_url  = $nt_store->getPostUrl( $id, null ) . $mediaDirName . '/';
-		$this->_meta = $nt_store->getPostDir( $id, null ) . 'media.json';
+		global $nt_store, $nt_config;
 
+		$this->_id    = $id;
+		$this->_dir   = $nt_store->getPostDir( $id, null ) . $mediaDirName . '/';
+		$this->_url   = $nt_store->getPostUrl( $id, null ) . $mediaDirName . '/';
+		$this->_meta  = $nt_store->getPostDir( $id, null ) . 'media.json';
 		$this->_ntUrl = $ntUrl;
+		$this->_sizes = $nt_config['image_sizes'];
+	}
+
+
+	// -------------------------------------------------------------------------
+
+
+	private function _loadMeta(): array {
+		if ( $this->_data ) {
+			return $this->_data;
+		}
+		$path = $this->_meta;
+		if ( is_file( $path ) && is_readable( $path ) ) {
+			$json = file_get_contents( $path );
+			if ( false === $json ) {
+				Logger::error( __METHOD__, 'Cannot read the meta data', $path );
+				return [];
+			}
+			$data = json_decode( $json, true );
+			if ( null === $data ) {
+				Logger::error( __METHOD__, 'The meta data is invalid', $path );
+			}
+			return $this->_data = $data ?? [];
+		}
+		return $this->_data = [];
+	}
+
+	private function _saveMeta( array $data ): void {
+		$this->_data = $data;
+
+		$json = json_encode( $data, self::JSON_OPTS );
+		$path = $this->_meta;
+		$res  = file_put_contents( $path, $json, LOCK_EX );
+		if ( false === $res ) {
+			Logger::error( __METHOD__, 'Cannot write the meta data', $path );
+			return;
+		}
+		@chmod( $path, NT_MODE_FILE );
 	}
 
 
@@ -42,24 +90,19 @@ class Media {
 
 		$list = [];
 		foreach ( $meta as $m ) {
-			$item = [];
-			$ext = $m['extension'];
-			if ( in_array( $ext, [ 'png', 'jpeg', 'jpg' ], true ) ) {
-				$item['is_image']   = true;
-				$item['sizes_json'] = json_encode( $this->_createSizesWithUrl( $m['sizes'] ), JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE );
-
-				if ( isset( $m['sizes']['small'] ) ) {
-					$item['url@min'] = $this->_mediaUrl( $m['sizes']['small']['file_name'] );
-				} else {
-					$item['url@min'] = $this->_mediaUrl( $m['sizes']['full']['file_name'] );
-				}
-			} else if ( $filter === 'image' ) {
+			if ( 'image' === $filter && ! in_array( $m['extension'], self::IMG_EXTS, true ) ) {
 				continue;
 			}
-			$item['file_name'] = $m['file_name'];
-			$item['url']       = $this->_mediaUrl( $m['file_name'] );
-			$item['ext']       = strtoupper( $ext );
-			$list[] = $item;
+			$sn = isset( $m['sizes'][ self::SIZE_NAME_MIN ] ) ? self::SIZE_NAME_MIN : self::SIZE_NAME_ORIG;
+
+			$list[] = [
+				'is_image'   => true,
+				'sizes_json' => json_encode( $this->_createSizesWithUrl( $m['sizes'] ), self::JSON_OPTS ),
+				'file_name'  => $m['file_name'],
+				'ext'        => strtolower( $m['extension'] ),  // Lowercasing for just in case.
+				'url'        => $this->_mediaUrl( $m['file_name'] ),
+				'url@min'    => $this->_mediaUrl( $m['sizes'][ $sn ]['file_name'] ),
+			];
 		}
 		return $list;
 	}
@@ -84,111 +127,39 @@ class Media {
 		return $this->_ntUrl . '?' . rawurlencode( $this->_id ) . '=' . rawurlencode( $fileName );
 	}
 
-	public function upload( array $file ): bool {
-		$tmpFile      = $file['tmp_name'];
-		$origFileName = $file['name'];
-
-		$fileName = \nt\get_unique_file_name( $this->_dir, $origFileName );
-		if ( empty( $fileName ) ) {
-			Logger::error( __METHOD__, 'File uploading failed', $fileName );
-			return false;
-		}
-		$path = $this->_dir . $fileName;
-		if ( is_uploaded_file( $tmpFile ) ) {
-			if ( ! is_dir( $this->_dir ) ) mkdir( $this->_dir, NT_MODE_DIR );
-			if ( is_dir( $this->_dir ) ) @chmod( $this->_dir, NT_MODE_DIR );
-
-			if ( move_uploaded_file( $tmpFile, $path ) ) {
-				@chmod( $path, NT_MODE_FILE );
-				$this->_addMeta( $fileName );
-				Logger::info( __METHOD__, 'File uploading succeeded', $fileName );
-				return true;
-			}
-		}
-		Logger::error( __METHOD__, 'File uploading failed', $fileName );
-		return false;
-	}
-
-	public function remove( string $fileName ): void {
-		$fileNames = $this->_removeMeta( $fileName );
-
-		foreach ( $fileNames as $fn ) {
-			$path = $this->_dir . $fn;
-			if ( ! is_file( $path ) ) continue;
-			$res = unlink( $path );
-			if ( $res === false ) {
-				Logger::error( __METHOD__, 'Cannot remove the file', $path );
-			}
-		}
-	}
-
 
 	// -------------------------------------------------------------------------
 
 
-	private function _loadMeta(): array {
-		if ( $this->_data ) return $this->_data;
+	public function remove( string $fileName ): void {
+		$fns = $this->_removeMeta( $fileName );
 
-		$path = $this->_meta;
-		if ( is_file( $path ) && is_readable( $path ) ) {
-			$json = file_get_contents( $path );
-			if ( $json === false ) {
-				Logger::error( __METHOD__, 'Cannot read the meta data', $path );
-				return [];
+		foreach ( $fns as $fn ) {
+			$path = $this->_dir . $fn;
+			if ( is_file( $path ) ) {
+				$res = unlink( $path );
+				if ( false === $res ) {
+					Logger::error( __METHOD__, 'Cannot remove the file', $path );
+				}
 			}
-			$data = json_decode( $json, true );
-			if ( $data === null ) {
-				Logger::error( __METHOD__, 'The meta data is invalid', $path );
-			}
-			return $this->_data = $data ?? [];
 		}
-		return $this->_data = [];
-	}
-
-	private function _saveMeta( array $data ): void {
-		$this->_data = $data;
-		$json = json_encode( $data, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE );
-
-		$path = $this->_meta;
-		$res = file_put_contents( $path, $json, LOCK_EX );
-		if ( $res === false ) {
-			Logger::error( __METHOD__, 'Cannot write the meta data', $path );
-			return;
-		}
-		@chmod( $path, NT_MODE_FILE );
-	}
-
-	private function _addMeta( string $fileName ): void {
-		$meta = $this->_loadMeta();
-
-		$ext = mb_strtolower( pathinfo( $fileName, PATHINFO_EXTENSION ) );
-		if ( in_array( $ext, [ 'png', 'jpeg', 'jpg' ], true ) ) {
-			list( $width, $height ) = getimagesize( $this->_dir . $fileName );
-			$meta[] = [
-				'file_name' => $fileName,
-				'extension' => $ext,
-				'sizes'     => $this->_resizeImage( $fileName, $ext )
-			];
-		} else {
-			$meta[] = [
-				'file_name' => $fileName,
-				'extension' => $ext
-			];
-		}
-		$this->_saveMeta( $meta );
 	}
 
 	private function _removeMeta( string $fileName ): array {
-		$meta = $this->_loadMeta();
+		$meta    = $this->_loadMeta();
 		$removed = [ $fileName ];
-		$idx = -1;
+		$idx     = -1;
 
 		foreach ( $meta as $i => $m ) {
-			if ( $m['file_name'] !== $fileName ) continue;
+			if ( $m['file_name'] !== $fileName ) {
+				continue;
+			}
 			$idx = $i;
 			if ( isset( $m['sizes'] ) ) {
 				foreach ( $m['sizes'] as $s => $d ) {
-					if ( isset( $d['file_name'] ) ) $removed[] = $d['file_name'];
+					if ( isset( $d['file_name'] ) ) {
+						$removed[] = $d['file_name'];
+					}
 				}
 			}
 			break;
@@ -200,22 +171,73 @@ class Media {
 		return $removed;
 	}
 
+	public function upload( array $file ): bool {
+		$tempFn = $file['tmp_name'];
+		$origFn = $file['name'];
+
+		$fn = \nt\get_unique_file_name( $this->_dir, $origFn );
+		if ( empty( $fn ) ) {
+			Logger::error( __METHOD__, 'File uploading failed', $fn );
+			return false;
+		}
+		$path = $this->_dir . $fn;
+		if ( is_uploaded_file( $tempFn ) ) {
+			if ( ! is_dir( $this->_dir ) ) {
+				mkdir( $this->_dir, NT_MODE_DIR );
+			}
+			if ( is_dir( $this->_dir ) ) {
+				@chmod( $this->_dir, NT_MODE_DIR );
+			}
+			if ( move_uploaded_file( $tempFn, $path ) ) {
+				@chmod( $path, NT_MODE_FILE );
+				$this->_addMeta( $fn );
+				Logger::info( __METHOD__, 'File uploading succeeded', $fn );
+				return true;
+			}
+		}
+		Logger::error( __METHOD__, 'File uploading failed', $fn );
+		return false;
+	}
+
+	private function _addMeta( string $fileName ): void {
+		$ext = mb_strtolower( pathinfo( $fileName, PATHINFO_EXTENSION ) );
+		$m   = [
+			'file_name' => $fileName,
+			'extension' => $ext
+		];
+		if ( in_array( $ext, self::IMG_EXTS, true ) ) {
+			$sizes = $this->_resizeImage( $fileName, $ext );
+			if ( $sizes ) {
+				$m['sizes'] = $sizes;
+			}
+		}
+		$meta   = $this->_loadMeta();
+		$meta[] = $m;
+		$this->_saveMeta( $meta );
+	}
+
 
 	// -------------------------------------------------------------------------
 
 
-	private function _resizeImage( string $fileName, string $ext ): array {
-		list( $width, $height ) = getimagesize( $this->_dir . $fileName );
-		$mime = mime_content_type( $this->_dir . $fileName );
+	private function _resizeImage( string $fileName, string $ext ): ?array {
+		$info = getimagesize( $this->_dir . $fileName );
+		if ( ! is_array( $info ) ) {
+			return null;
+		}
+		list( $width, $height ) = $info;
+		$sizes = [
+			self::SIZE_NAME_ORIG => [ 'file_name' => $fileName, 'width' => $width, 'height' => $height ],
+		];
 
-		global $nt_config;
-		$sizes = [];
-		$sizes['full'] = [ 'file_name' => $fileName, 'width' => $width, 'height' => $height ];
-
-		$min = intval( $nt_config['image_sizes']['small']['width'] );
+		$min = intval( $this->_sizes[ self::SIZE_NAME_MIN ]['width'] );
 		if ( $min < $width ) {
-			$img = $this->_loadImage( $fileName, $mime );
-			foreach ( $nt_config['image_sizes'] as $key => $d ) {
+			$mime = mime_content_type( $this->_dir . $fileName );
+			$img  = $this->_loadImage( $fileName, $mime );
+			if ( null === $img ) {
+				return $sizes;
+			}
+			foreach ( $this->_sizes as $key => $d ) {
 				$px = intval( $d['width'] );
 				if ( $px < $width ) {
 					// Replace hyphens with underscores for JSON.
@@ -232,38 +254,35 @@ class Media {
 
 	private function _saveScaledImage( $img, string $fn, string $mime, int $size ): array {
 		$newImg = $this->_scaleImage( $img, $size, $mime );
-
-		$mat = [ [ 0, -1, 0 ], [ -1, 12, -1 ], [ 0, -1, 0 ] ];
-		$div = 8;
-		imageconvolution( $newImg, $mat, $div, 0 );
-
-		$newFn = \nt\get_unique_file_name( $this->_dir, $fn, "-$size" );
+		$newFn  = \nt\get_unique_file_name( $this->_dir, $fn, "-$size" );
 		$this->_saveImage( $newFn, $mime, $newImg );
+
 		$ret = [ $newFn, imagesx( $newImg ), imagesy( $newImg ) ];
 		imagedestroy( $newImg );
 		return $ret;
 	}
 
 	private function _scaleImage( $img, int $newW, string $mime ) {
-		$w = imagesx( $img );
-		$h = imagesy( $img );
-		$newH = $h * $newW / $w;
+		$w      = imagesx( $img );
+		$h      = imagesy( $img );
+		$newH   = $h * $newW / $w;
 		$newImg = imagecreatetruecolor( $newW, $newH );
 
-		if ( $mime === 'image/png' || $mime === 'image/x-png' ) {
+		if ( 'image/png' === $mime || 'image/x-png' === $mime ) {
 			imagealphablending( $newImg, false );
 			imagefill( $newImg, 0, 0, imagecolorallocatealpha( $newImg, 0, 0, 0, 127 ) );
 			imagesavealpha( $newImg, true );
-		} else if ( $mime === 'image/gif' ) {
+		} elseif ( 'image/gif' === $mime ) {
 			$idx = imagecolortransparent( $img );
 			if ( 0 <= $idx ) {
-				$c = imagecolorsforindex( $img, $idx );
+				$c      = imagecolorsforindex( $img, $idx );
 				$newIdx = imagecolorallocate( $newImg, $c['red'], $c['green'], $c['blue'] );
 				imagefill( $newImg, 0, 0, $newIdx );
 				imagecolortransparent( $newImg, $newIdx );
 			}
 		}
 		imagecopyresampled( $newImg, $img, 0, 0, 0, 0, $newW, $newH, $w, $h );
+		imageconvolution( $newImg, self::FILTER_MAT, self::FILTER_DIV, 0 );
 		return $newImg;
 	}
 
