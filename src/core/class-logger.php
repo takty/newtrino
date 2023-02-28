f4320518639ecf9c75ffe61f8f6a1e43cd770fd4
Takuto Yanagida <yanagida@stxst.com>
2023-02-28 23:48:57 +0900

    Add checking of file permissions

--- src/core/class-logger.php
+++ src/core/class-logger.php
@@ -3,7 +3,7 @@
  * Logger
  *
  * @author Takuto Yanagida
- * @version 2021-06-18
+ * @version 2023-02-28
  */
 
 namespace nt;
@@ -39,14 +39,20 @@ class Logger {
 			$msg = var_export( $msg, true );
 		}
 		$path = self::LOG_FILE;
-		self::_ensureFile( $path );
+		if ( ! self::_ensureFile( $path ) ) {
+			if ( self::$debug ) {
+				self::_echoError();
+			}
+			return;
+		}
 		$fp = fopen( $path, 'ab+' );
-		if ( $fp === false ) return;
-
+		if ( false === $fp ) {
+			return;
+		}
 		flock( $fp, LOCK_EX );
 		set_file_buffer( $fp, 0 );
 		$line = join( ' ', [ date( '[Y-m-d H:i:s]' ), self::_createTimeHash(), $_SERVER['REMOTE_ADDR'], ucfirst( strtolower( $type ) ), $msg ] );
-		if ( self::$debug && $type !== 'info' ) {
+		if ( self::$debug && 'info' !== $type ) {
 			print( str_replace( "\n", "<br>\n", htmlspecialchars( $line, ENT_HTML5 ) ) . "<br>\n" );
 		}
 		fputs( $fp, "$line\n" );
@@ -54,14 +60,25 @@ class Logger {
 		fclose( $fp );
 	}
 
+	static private function _echoError() {
+		register_shutdown_function(
+			function () {
+				echo '<script>console.error("Logger cannot make any logs in the directory. Check file permissions.");</script>';
+			}
+		);
+	}
+
 	static private function _createTimeHash() {
 		$hash = hash( 'crc32b', $_SERVER['REQUEST_TIME_FLOAT'], true );
 		return rtrim( base64_encode( $hash ), '=' );
 	}
 
-	static private function _ensureFile( string $path ): void {
+	static private function _ensureFile( string $path ): bool {
 		if ( is_file( $path ) ) {
 			@chmod( $path, NT_MODE_FILE );
+			if ( null !== error_get_last() ) {
+				return false;
+			}
 			$fsize = filesize( $path );
 			if ( self::MAX_SIZE < $fsize ) {
 				$pi  = pathinfo( $path );
@@ -72,21 +89,30 @@ class Logger {
 			}
 		} else {
 			$dirLog = dirname( $path );
-			if ( ! is_dir( $dirLog ) ) mkdir( $dirLog, NT_MODE_DIR, true );
+			if ( ! is_dir( $dirLog ) ) {
+				@mkdir( $dirLog, NT_MODE_DIR, true );
+				if ( null !== error_get_last() ) {
+					return false;
+				}
+			}
 			if ( is_dir( $dirLog ) ) {
 				@chmod( dirname( $dirLog ), NT_MODE_DIR );
 				@chmod( $dirLog, NT_MODE_DIR );
 			}
 		}
+		return true;
 	}
 
 	static private function _rotateFile( string $dir, string $fn, string $ext ): void {
 		$n = self::FILE_NUM;
 		$path = "$dir{$fn}[$n]$ext";
-		if ( is_file( $path ) ) unlink( $path );
-
+		if ( is_file( $path ) ) {
+			unlink( $path );
+		}
 		for ( $i = $n - 1; $i > 0; $i -= 1 ) {
-			if ( ! is_file( "$dir{$fn}[$i]$ext" ) ) continue;
+			if ( ! is_file( "$dir{$fn}[$i]$ext" ) ) {
+				continue;
+			}
 			$j = $i + 1;
 			rename( "$dir{$fn}[$i]$ext", "$dir{$fn}[$j]$ext" );
 		}
