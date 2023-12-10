5a252ca1305225622b83a1216e4989a9da6df007
Takuto Yanagida <yanagida@stxst.com>
2023-12-10 22:42:02 +0900

    Update packages

--- gulp-task/_task-copy.mjs
+++ gulp-task/_task-copy.mjs
@@ -2,19 +2,19 @@
  * Function for gulp (Copy)
  *
  * @author Takuto Yanagida
- * @version 2022-08-19
+ * @version 2023-11-08
  */
 
 import gulp from 'gulp';
 import plumber from 'gulp-plumber';
 import ignore from 'gulp-ignore';
-import changed from 'gulp-changed';
+import changed, { compareContents } from 'gulp-changed';
 
 export function makeCopyTask(src, dest = './dist', base = null) {
 	const copyTask = () => gulp.src(src, { base: base })
 		.pipe(plumber())
 		.pipe(ignore.include({ isFile: true }))
-		.pipe(changed(dest, { hasChanged: changed.compareContents }))
+		.pipe(changed(dest, { hasChanged: compareContents }))
 		.pipe(gulp.dest(dest));
 	return copyTask;
 }
--- gulp-task/_task-sass.mjs
+++ gulp-task/_task-sass.mjs
@@ -2,7 +2,7 @@
  * Function for gulp (SASS)
  *
  * @author Takuto Yanagida
- * @version 2023-06-21
+ * @version 2023-11-08
  */
 
 const SASS_OUTPUT_STYLE = 'compressed';  // 'expanded' or 'compressed'
@@ -11,7 +11,7 @@ import gulp from 'gulp';
 import plumber from 'gulp-plumber';
 import autoprefixer from 'gulp-autoprefixer';
 import rename from 'gulp-rename';
-import changed from 'gulp-changed';
+import changed, { compareContents } from 'gulp-changed';
 
 import * as dartSass from 'sass';
 import gulpSass from 'gulp-sass';
@@ -30,7 +30,7 @@ export function makeSassTask(src, dest = './dist', base = null, addSuffix = true
 		.pipe(sass.sync({ outputStyle: SASS_OUTPUT_STYLE }))
 		.pipe(autoprefixer({ remove: false }))
 		.pipe(rename({ extname: addSuffix ? '.min.css' : '.css' }))
-		.pipe(changed(dest, { hasChanged: changed.compareContents }))
+		.pipe(changed(dest, { hasChanged: compareContents }))
 		.pipe(gulp.dest(dest, { sourcemaps: '.' }));
 	return sassTask;
 }
